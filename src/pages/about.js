cec863c4daadeaf97d448a984fe9044dc8f1dc2d
Dot <akdlwkd2@gmail.com>
2022-06-28 17:37:11 +0900

    블로그 수정본

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function AboutPage() {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        안녕하세요. 정영환입니다.
+      </p>
+    </Layout>
+  );
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Home Page</h1>
-    </main>
+    <Layout pageTitle="Home">
+      <p>Welcome my Blog</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/main.jpg" 
+      />
+    </Layout>
   );
 };
 
-export default IndexPage; 
+export default IndexPage;
